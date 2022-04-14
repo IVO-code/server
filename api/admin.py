d9cae4d479da5074fca856dce32b42f470274caf
George Mendonça <61426720+GeorgeOgeorge@users.noreply.github.com>
2022-04-14 17:20:01 -0300

    admin e serilizer criado

--- api/admin.py
+++ api/admin.py
@@ -2,8 +2,70 @@ from django.contrib import admin
 
 from .models import Atendimento, Card, Ocupacao, Paciente, Preceptor, Resposta, Roteiro
 
+
 @admin.register(Preceptor)
 class PreceptorAdmin(admin.ModelAdmin):
     list_display = [
-        
+        'ativo',
+        'login',
+        'senha',
+        'nome',
+    ]
+
+
+@admin.register(Ocupacao)
+class OcupacaoAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome'
+    ]
+
+
+@admin.register(Paciente)
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = [
+        'ativo',
+        'nome',
+    ]
+
+
+@admin.register(Roteiro)
+class RoteiroAdmin(admin.ModelAdmin):
+    list_display = [
+        'ativo',
+        'titulo',
+        'descricao',
+        'preceptor',
+    ]
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = [
+        'ativo',
+        'texto',
+        'figura',
+        'libras',
+        'audioDescricao',
+        'titulo',
+        'roteiro',
+    ]
+
+@admin.register(Resposta)
+class RespostaAdmin(admin.ModelAdmin):
+    list_display = [
+        'ativo',
+        'texto',
+        'figura',
+        'libras',
+        'audioDescricao',
+        'descricao',
+        'card',
+    ]
+
+@admin.register(Atendimento)
+class AtendimentoAdmin(admin.ModelAdmin):
+    list_display = [
+        'preceptor',
+        'paciente',
+        'roteiro',
     ]
---
+++ api/serializers.py
@@ -0,0 +1,3 @@
+from rest_framework import serializers
+
+from .models import Atendimento, Card, Ocupacao, Paciente, Preceptor, Roteiro, Resposta, 
