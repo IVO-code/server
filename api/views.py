4df39e254a344cbd3e674e26735e5cd7e2351762
Luan Luis Magioli <luanmagioli@gmail.com>
2022-07-21 08:57:05 -0300

    test

--- api/views.py
+++ api/views.py
@@ -64,11 +64,11 @@ class ElementoComunicativoViewSet(viewsets.ModelViewSet):
 class CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
-    
+
     def create(self, request):
         card = Card(
             titulo = ElementoComunicativo.objects.filter(pk=request.data['titulo']).get(),
-            descricao = ElementoComunicativo.objects.filter(pk=request.data['descricao'].get()),
+            descricao = ElementoComunicativo.objects.filter(pk=request.data['descricao']).get(),
             opcoes = list(ElementoComunicativo.objects.filter(pk__in=request.data['opcoes']))
         )
         card.save()
